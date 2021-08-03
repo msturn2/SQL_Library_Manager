1a61ebba31e879b06a814a3c742c52cfad9ea643
msturn2 <82980444+msturn2@users.noreply.github.com>
2021-08-02 18:09:34 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,3 @@ router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
-
-
-// const express = require('express');
-// const router = express.Router();
-
-// /* Get home page
-//    Redirect to routes/books.js
-// */
-// router.get('/', (req, res, next) => {
-//   res.redirect("/books")
-// });
-
-// module.exports = router;
